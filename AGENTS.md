cfd2ec9163a7137f3e000847eff907c946015210
Fate-Ku <elaina42fate@gmail.com>
2025-12-07 19:22:13 +0900

    2025/12/7 update logo

--- AGENTS.md
+++ AGENTS.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: StickerSimulator
 - Unity version: Unity 6000.0.43f1
-- Active game object:
-  - Name: StickerCard
-  - Tag: Untagged
-  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    public void TaskBTN()
+    {
+        //GameManager.instance.ChangeScene("Task");
+    }
+
+    public void MyBrandBTN()
+    {
+        GameManager.instance.ChangeScene("MyBrand");
+
+    }
+
+    public void StickerBookMenuBTN()
+    {
+        GameManager.instance.ChangeScene("StickerBookMenu");
+
+    }
+
+    public void StickerBookBTN()
+    {
+        GameManager.instance.ChangeScene("StickerBook");
+
+    }
+
+    public void ZukanBTN()
+    {
+        GameManager.instance.ChangeScene("Zukan");
+    }
+
+    public void GaChaBTN()
+    {
+        //GameManager.instance.ChangeScene("GaCha");
+
+    }
+
+    public void MainMenuBTN()
+    {
+        GameManager.instance.ChangeScene("MainMenu");
+
+    }
+
+
+    public void QuitGameBTN()
+    {
+        Application.Quit();
+    }
+}
