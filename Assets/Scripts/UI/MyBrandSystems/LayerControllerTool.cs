0f8401a50ade336a6dc1e9810b661e5c72545fbc
Fate-Ku <elaina42fate@gmail.com>
2026-01-28 22:55:04 +0900

    2026/1/28 update

---
+++ Assets/Scripts/UI/MyBrandSystems/LayerControllerTool.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class LayerControllerTool : MonoBehaviour
+{
+    // SelectTool スクリプトを参照する
+    public Select select;
+
+    // order in layer を 1 増やす
+    public void LayerUp()
+    {
+        if (select == null || select.targetRenderer == null)
+        {
+            Debug.Log("選択中のステッカーがありません");
+            return;
+        }
+
+        select.targetRenderer.sortingOrder += 1;
+        Debug.Log("Layer Up → " + select.targetRenderer.sortingOrder);
+    }
+
+    // order in layer を 1 減らす
+    public void LayerDown()
+    {
+        if (select == null || select.targetRenderer == null)
+        {
+            Debug.Log("選択中のステッカーがありません");
+            return;
+        }
+
+        int resultLayer = select.targetRenderer.sortingOrder - 1;
+
+        if (resultLayer < 0)
+        {
+            select.targetRenderer.sortingOrder = 0;
+        }
+        else
+        {
+            select.targetRenderer.sortingOrder -= 1;
+        }
+        Debug.Log("Layer Down → " + select.targetRenderer.sortingOrder);
+    }
+
+}
